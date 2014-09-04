bfb18022ff95ebf72f8e052835edd587bbe0fd8a
dogwood008 <dogwood008+github@gmail.com>
2014-09-04 20:41:25 +0900

    add accessor

--- lib/ruby2ch/thre.rb
+++ lib/ruby2ch/thre.rb
@@ -1,6 +1,10 @@
 class Thre
-  # @return [String] 板の名前
+  # @return [String] スレッドの名前
   attr_reader :title
+  # @return [String] スレッドキー(Unix time)
+  attr_reader :thread_key
+  # @return [int] 返信の数
+  attr_reader :num_of_response
 
   # @param [String] thread_data 0000000000.dat<>スレッドタイトル (レス数)
   def initialize(thread_data)
