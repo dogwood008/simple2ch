6650674c53aa20fbcf34591ddc76711fb8e34231
dogwood008 <dogwood008+github@gmail.com>
2014-09-05 13:08:42 +0900

    use Ruby2ch#fetch

--- lib/ruby2ch/board.rb
+++ lib/ruby2ch/board.rb
@@ -25,23 +25,20 @@ class Board
 
   private
   # URLが正しいかバリデーションする
-  # @param [String] url
+  # @param [URI] url
   def validate_url(url)
     sp_uri = URI.parse url
     board_url = ''
 
     if sp_uri.host.index '2ch'
       case url
-      when /http:\/\/(.+)\.2ch\.(net|sc)\/(.+)\/subject\.txt/
-        board_url = URI.parse("http://#{$1}.2ch.sc/#{$3}/")
-      when /http:\/\/(.+)\.2ch\.(net|sc)\/(.+)\//
-        board_url = URI.parse("http://#{$1}.2ch.sc/#{$3}/")
-      when /http:\/\/(.+)\.2ch\.(net|sc)\/(\w+)/
-        board_url = URI.parse("http://#{$1}.2ch.sc/#{$3}/")
-      when /http:\/\/(.+)\.2ch.(net|sc)\/test\/read.cgi\/(.+)\/([0-9]+)/
-        board_url = URI.parse("http://#{$1}.2ch.sc/#{$3}/")
-      when /http:\/\/(.+)\.2ch.(net|sc)\/(.+)\/dat\/([0-9]+)\.dat/
-        board_url = URI.parse("http://#{$1}.2ch.sc/#{$3}/")
+        when /http:\/\/(.+)\.2ch.(net|sc)\/test\/read.cgi\/(.+)\/([0-9]+)/,
+            /http:\/\/(.+)\.2ch\.(net|sc)\/(.+)\/subject\.txt/,
+            /http:\/\/(.+)\.2ch\.(net|sc)\/(.+)\//,
+            /http:\/\/(.+)\.2ch\.(net|sc)\/(\w+)/,
+            /http:\/\/(.+)\.2ch.(net|sc)\/(.+)\/dat\/([0-9]+)\.dat/
+          @server_name = $1; @board_name = $3
+          board_url = URI.parse("http://#{$1}.2ch.sc/#{$3}/")
       else
         raise NotA2chUrlException
       end
@@ -55,15 +52,11 @@ class Board
   # 板に属する全てのスレッドをsubject.txtから取得する
   # @return [Array<Thre>] 板に属する全てのスレッド
   def fetch_all_thres
-    require 'net/http'
     subject_url = @url+'subject.txt'
-    req = Net::HTTP::Get.new(subject_url.path)
-    res = Net::HTTP.start(subject_url.host, subject_url.port) {|http|
-      http.request(req)
-    }
-    subject_txt = res.body.force_encoding("cp932")
-    subject_txt.encode!('utf-8', :undef => :replace).each_line do |line|
-      @thres << Thre.new(line)
+
+    subject_txt = Ruby2ch.fetch(subject_url)
+    subject_txt.each_line do |line|
+      @thres << Thre.new(self, line)
     end
     @thres
   end
