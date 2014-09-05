87050ae9903c37b9dac3fb0cdaca32189bde9ccf
dogwood008 <dogwood008+github@gmail.com>
2014-09-05 13:07:52 +0900

    add Ruby2ch#fetch

--- lib/ruby2ch.rb
+++ lib/ruby2ch.rb
@@ -6,9 +6,21 @@ module Ruby2ch
   require 'ruby2ch/dat'
   require 'ruby2ch/res'
   require 'ruby2ch/thre'
+  require 'net/http'
   # Your code goes here...
   def self.root
     File.dirname __dir__
+  end
+
+  # HTTPでGETする
+  # @param [URI] URL
+  # @return [String] 取得本文
+  def self.fetch(url)
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    res.body.force_encoding("cp932").encode!('utf-8', :undef => :replace)
   end
 end
 
