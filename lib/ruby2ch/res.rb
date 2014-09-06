98fc5b048e1f5ad3b7e9d87e484c1b7543fc9063
dogwood008 <dogwood008+github@gmail.com>
2014-09-06 15:52:59 +0900

    add parse Error and Kako log error

--- lib/ruby2ch/res.rb
+++ lib/ruby2ch/res.rb
@@ -12,6 +12,7 @@ class Res
   # @return [String] 内容
   attr_reader :contents
 
+  KAKO_LOG_INFO = '過去ログ ★<><>[過去ログ]<><em>■ このスレッドは過去ログ倉庫に格納されています</em><>'
 
   def initialize(res_num, author: '', author_id: '', date: nil, mail: '', contents: '')
     @res_num = res_num
@@ -22,11 +23,17 @@ class Res
     @contents = contents
   end
 
+  # @raise [KakoLogException] 過去ログ情報をパースしようとした際に発生
   def self.parse(res_num, contents)
-    self.new res_num, self.parse_dat(contents)
+    unless contents.strip == KAKO_LOG_INFO
+      self.new res_num, self.parse_dat(contents)
+    else
+      raise KakoLogException
+    end
   end
 
   private
+  # @raise [DatParseException] Datのパースに失敗したときに発生
   def self.parse_dat(dat)
     split_date_and_id_regex = /(^\d{4}\/\d{2}\/\d{2}\(.\) \d{2}:\d{2}:\d{2}\.\d{2}) ID:(\S+)$/
     ret = {}
@@ -37,6 +44,9 @@ class Res
     ret[:contents] = split[3].strip!
 
     date_and_author_id =~ split_date_and_id_regex
+    if !$1 || !$2
+      raise DatParseException
+    end
     ret[:date] = Time.parse $1
     ret[:author_id] = $2
 
--- lib/ruby2ch/ruby2ch_exception.rb
+++ lib/ruby2ch/ruby2ch_exception.rb
@@ -1,2 +1,4 @@
 class Ruby2chException < Exception; end
 class NotA2chUrlException < Ruby2chException; end
+class KakoLogException < Ruby2chException; end
+class DatParseException < Ruby2chException; end
