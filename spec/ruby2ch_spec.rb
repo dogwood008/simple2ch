56da2922ee6704b2fd93f32d58d360350f839ef1
dogwood008 <dogwood008+github@gmail.com>
2014-09-06 21:05:13 +0900

    add Ruby2ch spec

---
+++ spec/ruby2ch_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+require 'spec_helper'
+
+describe Ruby2ch do
+  context 'should get reses from board url' do
+    before(:all) do
+      board_name = 'ニュー速VIP'
+      board_url = 'http://viper.2ch.sc/news4vip/'
+      @board = Board.new board_name, board_url
+      @threads= @board.threads
+      @res = @threads[0].reses[0]
+    end
+    it{ expect(@board.threads).to be_a_kind_of Array }
+    it do
+      #@threads = board.threads
+      expect(@board.threads.size).to be > 0
+    end
+
+    it { expect(@threads[0]).to be_a_kind_of Thre }
+    it { expect(@threads[0].reses).to be_a_kind_of Array }
+
+    it do
+      #@res = @threads[0].reses[0]
+      expect(@res).to be_a_kind_of Res
+    end
+    it { expect(@res.date).to be < Time.now }
+    it { expect(@res.author_id.size).to be > 0 }
+
+  end
+
+
+#its(:reses){ is_expected.to be a_kind_of Array}
+end
