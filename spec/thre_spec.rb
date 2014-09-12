b199b50b595ede122d0b7339eecdbe089919997c
dogwood008 <dogwood008+github@gmail.com>
2014-09-12 19:59:24 +0900

    Thre#reses become able to have which reses to extract. e.g. Thre#reses([1,2,3])

--- spec/thre_spec.rb
+++ spec/thre_spec.rb
@@ -34,4 +34,27 @@ describe Simple2ch::Thre do
     subject { thre.kako_log? }
     it { is_expected.to be_truthy }
   end
+
+  describe '#reses' do
+    let(:board) { Simple2ch::Board.new 'ニュース速報(VIP)', 'http://viper.2ch.sc/news4vip/'}
+    let(:dat_data) { '1409796283.dat<>Ｃ言語の勉強始めたんだがな (144)' }
+    let(:thre) { Simple2ch::Thre.parse(board, dat_data) }
+
+    shared_examples 'have specified reses' do
+      subject{thre.reses(specified_reses)}
+      it{is_expected.to be_a_kind_of Array}
+      its(:size){is_expected.to be == size}
+    end
+    context 'when without res_num' do
+      let(:size){144}
+      let(:specified_reses){nil}
+      it_behaves_like 'have specified reses'
+    end
+
+    context 'when with res_num' do
+      let(:size){3}
+      let(:specified_reses){[1,2,10]}
+      it_behaves_like 'have specified reses'
+    end
+  end
 end
