2f6d5d566f791e69f6a9682046f8c8a4d7665c73
dogwood008 <dogwood008+github@gmail.com>
2014-09-12 19:57:17 +0900

    replace "context" with "describe"

--- spec/thre_spec.rb
+++ spec/thre_spec.rb
@@ -6,31 +6,31 @@ describe Simple2ch::Thre do
   let(:dat_data) { '1409796283.dat<>Ｃ言語の勉強始めたんだがな (144)' }
   let(:thre) { Simple2ch::Thre.parse(board, dat_data) }
 
-  context 'should have title' do
+  describe 'should have title' do
     subject { thre.title }
     it { is_expected.to be_a_kind_of(String) }
   end
 
-  context 'should have thread key' do
+  describe 'should have thread key' do
     subject { thre.thread_key }
     it { is_expected.to be_a_kind_of(String) }
     it { is_expected.to match /\d{10}/ }
   end
 
-  context 'should have numbers of responses' do
+  describe 'should have numbers of responses' do
     subject { thre.num_of_response }
     it { is_expected.to be_a_kind_of(Fixnum) }
     it { is_expected.to be > 0 }
   end
 
-  context 'should have responses' do
+  describe 'should have responses' do
     subject { thre.reses }
     it { is_expected.to be_a_kind_of(Array) }
     it { subject.each{ |r| expect(r).to be_a_kind_of(Simple2ch::Res) } }
     its(:size) { is_expected.to be > 0 }
   end
 
-  context 'should have if Kako log' do
+  describe 'should have if Kako log' do
     subject { thre.kako_log? }
     it { is_expected.to be_truthy }
   end
