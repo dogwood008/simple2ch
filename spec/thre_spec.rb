59c61c4ec22d00a80bf9112ee0e8e39fff70a182
dogwood008 <dogwood008+github@gmail.com>
2014-09-13 16:56:18 +0900

    Refactoring: remove unused codes.

--- spec/thre_spec.rb
+++ spec/thre_spec.rb
@@ -36,10 +36,6 @@ describe Simple2ch::Thre do
   end
 
   describe '#reses' do
-    let(:board) { Simple2ch::Board.new 'ニュース速報(VIP)', 'http://viper.2ch.sc/news4vip/'}
-    let(:dat_data) { '1409796283.dat<>Ｃ言語の勉強始めたんだがな (144)' }
-    let(:thre) { Simple2ch::Thre.parse(board, dat_data) }
-
     shared_examples 'have specified reses' do
       subject{thre.reses(specified_reses)}
       it{is_expected.to be_a_kind_of Array}
