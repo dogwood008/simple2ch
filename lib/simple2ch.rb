0aca4f3087e41bac3c7a77c79d564416bcd75073
dogwood008 <dogwood008+github@gmail.com>
2015-03-03 18:45:50 +0900

    [add] Add function "fetch all boards"

--- lib/simple2ch.rb
+++ lib/simple2ch.rb
@@ -17,15 +17,48 @@ module Simple2ch
     File.dirname __dir__
   end
 
+  # Module variables
+  @@bbsmenu_url = ''
+  @@boards = []
+
   # HTTPでGETする
   # @param [URI] url URL
+  # @param [Symbol] site :net, :sc, :openのいずれか．(2ch.net or 2ch.sc or open2ch.net)
   # @return [String] 取得本文
-  def self.fetch(url)
+  def self.fetch(url, site=:sc)
     req = Net::HTTP::Get.new(url.path)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
     }
-    res.body.force_encoding("cp932").encode!('utf-8', :undef => :replace)
+    case site
+      when :net, :sc
+        res.body.force_encoding("cp932").encode!('utf-8', :undef => :replace)
+      when :open
+        res.body.force_encoding("utf-8")
+    end
+  end
+
+  # bbsmenuのURLが渡されればセットして，板リストを返す
+  # @param [String] bbsmenu_url bbs_menuのURL
+  # @param [Symbol] site :net, :sc, :openのいずれか．(2ch.net or 2ch.sc or open2ch.net)
+  # @return [Array<Simple2ch::Board>] 板リスト
+  def self.board_lists(bbsmenu_url=nil, site=nil)
+    if bbsmenu_url
+      @@bbsmenu_url = bbsmenu_url
+      # http://www.rubular.com/r/u1TJbQAULD
+      board_extract_regex = /<A HREF=http:\/\/(?<subdomain>\w+).(?<openflag>open|)2ch.(?<tld>sc|net)\/(?<board_name>\w+)\/>(?<board_name_ja>.+)<\/A>/
+
+      data = nil
+      boards_array = []
+      raise RuntimeError, "Failed to fetch #{url}" if (data = fetch(URI.parse(@@bbsmenu_url), site)).empty?
+      raise RuntimeError, "Failed to parse #{url}" if (boards_array=data.scan(board_extract_regex).uniq).empty?
+    end
+
+    @@boards = []
+    boards_array.each do |b|
+      @@boards << Simple2ch::Board.new(b[4],"http://#{b[0]}.#{b[1]}2ch.#{b[2]}/#{b[3]}/")
+    end
+    @@boards
   end
 end
 
--- spec/simple2ch_spec.rb
+++ spec/simple2ch_spec.rb
@@ -1,7 +1,28 @@
 require 'rspec'
 require 'spec_helper'
 
+RSpec::Matchers.define :have_news4vip do
+  match do |boards|
+    boards.find{|b| b.title == 'ニュー速VIP'}
+  end
+end
+
 describe Simple2ch do
+  describe 'should get board from board list' do
+    let(:board_list_url) { {net: 'http://menu.2ch.net/bbsmenu.html', sc: 'http://2ch.sc/bbsmenu.html', open: 'http://open2ch.net/menu/pc_menu.html' } }
+    shared_examples 'get board list from bbsmenu' do
+      subject{ Simple2ch.board_lists(bbsmenu_url, site) }
+      it{ is_expected.not_to be_empty }
+      it{ is_expected.to have_news4vip}
+    end
+
+    context 'from 2ch.net' do
+      let(:bbsmenu_url) { board_list_url[:net] }
+      let(:site) { :net }
+      include_examples 'get board list from bbsmenu'
+    end
+  end
+
   context 'should get reses from board url' do
     before(:all) do
       board_name = 'ニュー速VIP'
