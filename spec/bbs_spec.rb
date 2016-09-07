c8a38e801c45a8842ee03346856ddc1981616df1
dogwood008 <dogwood008+github@gmail.com>
2016-09-08 00:30:00 +0900

    :+1: Use vcr

--- spec/bbs_spec.rb
+++ spec/bbs_spec.rb
@@ -1,62 +1,64 @@
 require 'rspec'
 require 'spec_helper'
 
-describe Simple2ch::BBS do
-  before(:all) do
-    @sc = Simple2ch::BBS.new(:sc)
-    @open = Simple2ch::BBS.new(:open)
-  end
+VCR.use_cassette 'bbs' do
+  describe Simple2ch::BBS, vcr: true do
+    before(:all) do
+      @sc = Simple2ch::BBS.new(:sc)
+      @open = Simple2ch::BBS.new(:open)
+    end
 
-  describe '#get_boards_by_type_of_2ch' do
-    shared_examples '#get_boards_by_type_of_2ch' do
+    describe '#get_boards_by_type_of_2ch' do
+      shared_examples '#get_boards_by_type_of_2ch' do
+        shared_examples 'get board list from bbsmenu' do
+          subject { bbs.get_boards_by_type_of_2ch(type_of_2ch, force_reload: force_reload) }
+          it { expect(bbs.type_of_2ch).to eq type_of_2ch }
+          it { is_expected.not_to be_empty }
+          it { is_expected.to have_news4vip }
+        end
+
+        context 'from 2ch.sc' do
+          include_examples 'get board list from bbsmenu' do
+            let(:bbs) { @sc }
+            let(:type_of_2ch) { :sc }
+          end
+        end
+        context 'from open2ch.net' do
+          include_examples 'get board list from bbsmenu' do
+            let(:bbs) { @open }
+            let(:type_of_2ch) { :open }
+          end
+        end
+      end
+
+      context 'use cache' do
+        let(:force_reload) { false }
+        it_behaves_like '#get_boards_by_type_of_2ch'
+      end
+      context 'use force reload' do
+        let(:force_reload) { true }
+        it_behaves_like '#get_boards_by_type_of_2ch'
+      end
+    end
+
+    describe '#boards' do
       shared_examples 'get board list from bbsmenu' do
-        subject { bbs.get_boards_by_type_of_2ch(type_of_2ch, force_reload: force_reload) }
+        subject { bbs.boards }
         it { expect(bbs.type_of_2ch).to eq type_of_2ch }
         it { is_expected.not_to be_empty }
         it { is_expected.to have_news4vip }
       end
 
       context 'from 2ch.sc' do
-        include_examples 'get board list from bbsmenu' do
-          let(:bbs) { @sc }
-          let(:type_of_2ch) { :sc }
-        end
+        let(:bbs) { @sc }
+        let(:type_of_2ch) { :sc }
+        include_examples 'get board list from bbsmenu'
       end
       context 'from open2ch.net' do
-        include_examples 'get board list from bbsmenu' do
-          let(:bbs) { @open }
-          let(:type_of_2ch) { :open }
-        end
+        let(:bbs) { @open }
+        let(:type_of_2ch) { :open }
+        include_examples 'get board list from bbsmenu'
       end
-    end
-
-    context 'use cache' do
-      let(:force_reload) { false }
-      it_behaves_like '#get_boards_by_type_of_2ch'
-    end
-    context 'use force reload' do
-      let(:force_reload) { true }
-      it_behaves_like '#get_boards_by_type_of_2ch'
-    end
-  end
-
-  describe '#boards' do
-    shared_examples 'get board list from bbsmenu' do
-      subject { bbs.boards }
-      it { expect(bbs.type_of_2ch).to eq type_of_2ch }
-      it { is_expected.not_to be_empty }
-      it { is_expected.to have_news4vip }
-    end
-
-    context 'from 2ch.sc' do
-      let(:bbs) { @sc }
-      let(:type_of_2ch) { :sc }
-      include_examples 'get board list from bbsmenu'
-    end
-    context 'from open2ch.net' do
-      let(:bbs) { @open }
-      let(:type_of_2ch) { :open }
-      include_examples 'get board list from bbsmenu'
     end
   end
 end
